c50d52b6bbd135b4fd2198667e0550344fac61fe
Azazel000 <>
2021-12-10 10:39:24 +0000

    main?

---
+++ src/lib/main.py
@@ -0,0 +1,17 @@
+import cmath
+import math
+
+pi=math.pi
+#z=cmath.exp(1j/pi)
+
+#print(z)
+
+r=input("Inserisci il raggio:\n")
+r=float(r)
+
+a=pi*r**2
+p=2*pi*r
+print(a,p)
+
+b=r'Hello \t World!'
+print(b)
